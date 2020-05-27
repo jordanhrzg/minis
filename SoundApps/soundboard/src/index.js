111567e11d18ea8be2e39ee7433f029d8c0e717e
Jordan Herzog <jaherzog@asu.edu>
2020-05-27 14:54:43 -0400

    lifted state to render playing buttons

--- SoundApps/soundboard/src/index.js
+++ SoundApps/soundboard/src/index.js
@@ -6,70 +6,34 @@ const axios = require('axios');
 
 //ref: https://samples.landr.com/packs/pantone-classic-blue-sample-pack?creator=eyJ2ZXJzaW9uIjoxLCJicG0iOjEyMCwia2V5Um9vdCI6IkMiLCJrZXlRdWFsaXR5IjoibWFqb3IiLCJzYW1wbGVzIjpbXX0%3D
 
-export default class AudioButton extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            name: props.name,
-            fileLocation: props.location,
-            track: null,
-            playing: false
-        };
-    }
+function calculateLocation(html){
+    let result = ['COY'];
+    // console.log(html);
 
-    async handleClick(){
-        if(this.state.track === null || !this.state.playing){
-            console.log(`Getting ${this.state.fileLocation} from server & looping play ...`);
-
-            //ref: https://developer.mozilla.org/en-US/docs/Web/Guide/Audio_and_video_delivery
-            let context = new AudioContext();
-            let request = new XMLHttpRequest();
-            let source = null;
-            request.open('GET', `http://localhost:3001/track?location=${this.state.fileLocation}`, true);
-            request.responseType = 'arraybuffer';
-            request.onload = function(){
-                context.decodeAudioData(request.response, function(buffer){
-                    source = context.createBufferSource();
-                    source.buffer = buffer;
-                    source.connect(context.destination);
-                    source.start(0);
-                    source.loop = true;
-                })
-            };
-            request.send();
-
-            this.state.track = context;
-            this.state.playing = true;
-
-        } else {
-            console.log(`Pausing ${this.state.fileLocation} ...`);
-            this.state.track.suspend();
-            this.state.playing = false;
+    let parts = html.replace(/<br>/g, ' ');
+    parts = parts.split(' ');
+    for(let part of parts){
+        if(part !== ''){
+            result.push(part);
         }
     }
-
-    render() {
-        return(
-            <button className={(this.state.playing) ? "playing" : "paused"} key={this.state.fileLocation} onClick={async () => await this.handleClick()}>
-                {this.state.name.name}
-                <br />
-                {this.state.name.bpm}
-                <br />
-                {this.state.name.key}
-            </button>
-        );
-    }
+    result = result.join('_');
+    result += '.wav';
+    // console.log(result);
+    return result;
 }
 
-function Board(props){
-    let buttonArray = [];
-    for(let file of props.audioFiles) {
-        buttonArray.push(new AudioButton(file));
-    }
+
+function AudioButton(props) {
+    let className = (props.playing) ? "playing": "paused";
     return (
-        <div className={"button-array"}>
-            {buttonArray.map(button => button.render())}
-        </div>
+        <button className={className} key={props.location} onClick={props.onClick}>
+            {props.name.name}
+            <br />
+            {props.name.bpm}
+            <br />
+            {props.name.key}
+        </button>
     );
 }
 
@@ -91,22 +55,92 @@ class SoundBoard extends React.Component {
 
             console.log(`Received ${results.length} track listings from server.`);
 
+            results = results.map(result => {
+                result.playing = false;
+                result.track = null;
+                return result;
+            });
+
             this.setState({
                 audio: results
             });
         }
     }
 
+    async handleClick(i){
+        let audioArray = this.state.audio;
+        let trackLocation = calculateLocation(i.target.innerHTML);
+
+        if(i.target.className === 'paused'){
+            console.log(`Getting ${trackLocation} from server & looping play ...`);
+
+            //ref: https://developer.mozilla.org/en-US/docs/Web/Guide/Audio_and_video_delivery
+            let context = new AudioContext();
+            let request = new XMLHttpRequest();
+            let source = null;
+            request.open('GET', `http://localhost:3001/track?location=${trackLocation}`, true);
+            request.responseType = 'arraybuffer';
+            request.onload = function(){
+                context.decodeAudioData(request.response, function(buffer){
+                    source = context.createBufferSource();
+                    source.buffer = buffer;
+                    source.connect(context.destination);
+                    source.start(0);
+                    source.loop = true;
+                })
+            };
+            request.send();
+
+            audioArray.some((obj) => {
+                if(obj.location === trackLocation){
+                    obj.track = context;
+                    obj.playing = true;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+        } else {
+            console.log(`Pausing ${trackLocation} ...`);
+
+            audioArray.some(function(obj){
+                if(obj.location === trackLocation){
+                    obj.track.suspend();
+                    obj.playing = false;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        }
+
+        this.setState({
+            audio: audioArray
+        });
+    }
+
     render(){
+        let buttonArray = [];
+        for(let file of this.state.audio) {
+            buttonArray.push(
+                <AudioButton
+                    name={file.name}
+                    playing={file.playing}
+                    key={file.location}
+                    onClick={async (i) => await this.handleClick(i)}
+                />);
+        }
+
         return (
             <div className="sound">
                 <script>
                     {JSON.stringify(this.soundCall())}
                 </script>
                 <h1>Sound Board Testing</h1>
-                <Board
-                    audioFiles={this.state.audio}
-                />
+                <div className={"button-array"}>
+                    {buttonArray}
+                </div>
             </div>
         );
     }
